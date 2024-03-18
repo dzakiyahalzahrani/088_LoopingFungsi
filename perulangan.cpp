f2e429668f1bbcefc8c566b8bd0a7901ad3df88b
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-03-18 14:42:18 +0700

    perulangan for

---
+++ perulangan.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int arr[5];
+
+    for (i =0;i<5;i++)
+    {
+
+        cout << i << ":" << "Dzakyh" << endl;
+    }
+
+    for (i = 0; i < 5; i ++)
+    {
+        cout << "Masukan Nama =";
+        cin >> arr[i];
+    }
+
+    for (i = 0; i< 5; i++)
+    {
+        cout << "Nama ke - " << i << " : " << arr[i] << endl;
+    }
+
+    
+}
