48e0d18ce94297b9f08fe6d9ad1ea5b9c905d665
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-03-18 14:59:24 +0700

    perulangan Do While

---
+++ perulanganDoWhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char pilihan;
+
+    do
+    {
+        cout << "berangkat ambil takjil" << endl;
+        cout << "antri takjil" << endl;
+        cout << "ambil takjil" << endl;
+        cout << "apakah mau antri lagi?" << endl;
+        cin >> pilihan;
+
+    }
+    while(pilihan == 'y' || pilihan == 'Y');
+}
